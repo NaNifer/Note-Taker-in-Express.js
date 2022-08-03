79c24598256fcde0b7928431f5bfef8a103b7f08
Nifer Kilakila <nifercritter@gmail.com>
2022-08-03 10:42:48 -0700

    Started routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,50 @@
 const fs = require('fs');
 const router = require('express').Router();
 const util = require('util');
+const { v4: uuidv4 } = require("uuid");
+
+const notes = require("../db/db.json");
+const { json } = require("express");
+
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require("../helpers/fsUtils");
 
 
+// GET Route for retrieving all the notes
+router.get("/notes", (req, res) => {
+  readFromFile("db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new notes
+router.post("/notes", (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    notes.push(newNote);
+    writeToFile("db/db.json", notes);
+    res.json(notes);
+    console.log("Note Added!");
+  } else {
+    res.error("Whoops, there was an error adding the note!");
+  }
+});
 
 
+// DELETE note route
+router.delete("/notes/:id", (req, res) => {
+  const noteId = req.params.id; 
+  // Array of all notes minus the URL id note
+  const notesKeep = notes.filter((note) => note.id !== noteId);
+  writeToFile("db/db.json", notesKeep);
+  res.json(notesKeep);
+});
 
 module.exports = router;
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const path = require("path");
+const apiRoutes = require('../apiRoutes')
+
+router.use('/api', apiRoutes);
+
+//GET /notes should return the notes.html file
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
